f5e70e957b468e40c0c01e2e3e98c53645f58fff
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 22:05:50 +0200

    day.js array moving
    moved days array inside a function

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,10 +2,8 @@
 
 'use strict';
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 const getDayNumber = (dayToFind) => {
-  
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   for (let i = 0; i < days.length; i++) {
     if (dayToFind.startsWith(days[i].toLowerCase())) {
       return i + 1;
