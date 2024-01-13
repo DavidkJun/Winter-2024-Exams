da1a48ad2d4c0d1bd2da4e1c59fd663a9331ca1c
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 23:42:55 +0200

    filter.js simplified code
    simplified code with removing unneded operations and variebles

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,14 @@
 'use strict';
 
 const filter = (array, typeOfElement) => {
-  const removed = [];
+  const filteredArray = [];
   for (const element of array) {
     const index = array.indexOf(element);
-      if (typeof array[index] !== typeOfElement) {
-    removed.unshift(index);
+    if (typeof array[index] === typeOfElement) {
+      filteredArray.push(array[index]);
     }
   }
-  for (const index of removed) array.splice(index, 1);
-  return array;
+  return filteredArray;
 };
 
 module.exports = filter;
