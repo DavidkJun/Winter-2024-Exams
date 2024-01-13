fd6c818cc809e87ed804b90379d817a71da067fb
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-14 00:33:38 +0200

    duplicate.js imroved code structure
    imroved code structure for better readability

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,8 +2,9 @@
 'use strict';
 
 const duplicate = (valueToRepeat, timesToRepeat) => {
-  if (timesToRepeat <= 0) return [];
-  else {
+  if (timesToRepeat <= 0) {
+    return [];
+  } else {
     const duplicates = [];
     for (let i = 0; i < timesToRepeat; i++) {
       duplicates[i] = valueToRepeat;
