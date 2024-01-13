7bfaf53e6ceee0a50e827f6c9a6bb31b985887e5
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 23:03:39 +0200

    skip.js added array
    added array for the results

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -2,6 +2,7 @@
 'use strict'
 
 const skip = (array, ...elementsToRemove) => {
+  const result = [];
   const index = 0;
   for (const value of array) {
     for (const elementToRemove of elementsToRemove) {
