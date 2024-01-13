76e3af693041344187d89ac607d95401c96f0a42
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 22:00:44 +0200

    day.js improved naming
    changed function and it's parametr name, and name of days array

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,16 +2,16 @@
 
 'use strict';
 
-const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const _parse_day_ = (s) => {
+const getDayNumber = (dayToFind) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (dayToFind.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = getDayNumber;
