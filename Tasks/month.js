e2d77ae4bfe92e205c2a4354f599fc2de2446228
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 22:43:24 +0200

    moths.js changing code structure
    put the return statement into braces for better readability

--- Tasks/month.js
+++ Tasks/month.js
@@ -5,7 +5,9 @@ const months = ['jan','feb', 'mar','apr','may','jun','jul','aug','sep','oct','no
 const findMonth = (monthToFind) => {
   const numberOfMonths = months.length;
   for (let i = 0; i < numberOfMonths; i++) {
-    if (monthToFind.toLowerCase().startsWith(months[i])) return i + 1;
+    if (monthToFind.toLowerCase().startsWith(months[i])) {
+      return i + 1;
+    }
   }
   return -1;
 };
