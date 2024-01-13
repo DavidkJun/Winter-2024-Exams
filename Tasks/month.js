742afd4cddde4567d8676b294e0f3e8826a8e37f
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-13 22:39:51 +0200

    month.js improve naming
    changed name of variable for the number of months

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,10 +2,9 @@
 'use strict'
 
 const months = ['jan','feb', 'mar','apr','may','jun','jul','aug','sep','oct','nov', 'dec',];
-
 const findMonth = (monthToFind) => {
-  const l = months.length;
-  for (let i = 0; i < l; i++) {
+  const numberOfMonths = months.length;
+  for (let i = 0; i < numberOfMonths; i++) {
     if (monthToFind.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
