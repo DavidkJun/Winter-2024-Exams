d3cbfbe3131b6b8cbe5fc7e9b0487cd2a9c0ae01
Davyd Krykota <krikotadavid957@gmail.com>
2024-01-16 15:31:49 +0200

    day.js fixed
    used intemidiate variables

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,13 +1,13 @@
 // Get day number
 
 'use strict';
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (dayToFind) => {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  for (let i = 0; i < days.length; i++) {
-    if (dayToFind.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+  const weekLength = days.length;
+  for (let i = 0; i < weekLength; i++) {
+    const loweredDay = days[i].toLowerCase();
+    if (dayToFind.startsWith(loweredDay)) return i + 1; 
   }
   return -1;
 };
